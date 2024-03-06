395adf29b1ce1392fd47d6e982806bc49f2cf353
Per Bergström <per.bergstrom@fore.dev>
2024-03-06 08:47:43 +0100

    Update Header component styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,15 +2,14 @@
 	import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
-<div class="navbar bg-base-100">
+<div class="navbar sticky top-0 z-50 bg-base-100/50 backdrop-blur">
 	<div class="flex-1">
-		<a href="/" class="btn btn-ghost text-xl">Varberg Energi</a>
+		<a href="/" class="btn btn-ghost text-xl">Header</a>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal items-center gap-4 px-1">
 			<li><a href="/">Link</a></li>
 			<li>
-				<!-- <button class="btn btn-sm glass" onclick={window.toggleTheme}>Toggle theme</button> -->
 				<ThemeToggle />
 			</li>
 		</ul>
